1703d59232811ec75409ee610d81906d8af17361
dattila90 <demeterattila90@gmail.com>
2019-12-12 23:01:33 +0100

    a little bit more offical

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import PostList from './PostList'
 const App = () => {
     return (
         <div className='ui container'>
+            <h1>Blog React App</h1>
             <PostList />
         </div>
     );
