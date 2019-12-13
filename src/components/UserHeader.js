c65f805c438cbd54ff866594950457ceec2b0d6d
dattila90 <demeterattila90@gmail.com>
2019-12-13 15:43:53 +0100

    alternate request optimization

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
     
     render() {
         const { user } = this.props;
@@ -22,7 +18,4 @@ const maspStateToProps = (state, ownProps) => {
     return { user: state.users.find(user => user.id === ownProps.userId) };
 };
 
-export default connect(
-    maspStateToProps, 
-    { fetchUser }
-)(UserHeader);
+export default connect(maspStateToProps)(UserHeader);
